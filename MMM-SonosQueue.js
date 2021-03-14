550a6298b7fc5f207f068cf8dd2220707303e026
Brady Snelson <bsnelson@umd.edu>
2021-03-13 20:14:54 -0500

    update visuals

--- MMM-SonosQueue.js
+++ MMM-SonosQueue.js
@@ -15,13 +15,11 @@ Module.register("MMM-SonosQueue",{
 	
     defaults: {
         maxLength: 10,
-        minWidth: "400px",
-        minHeight: "50px",
 		serverIP: "http://localhost:5005",
         queueUpdateInterval: 5, // 5 seconds
-        statusUpdateInterval: 1,
-		room: "Living Room",
-        updateExternally: true,
+        statusUpdateInterval: 2,
+		room: "Bathroom",
+        updateExternally: false,
         broadcastStatus: false,
     },
 
@@ -32,12 +30,16 @@ Module.register("MMM-SonosQueue",{
 
     start: function() {
         Log.info('Starting module: ' + this.name + ', version ' + this.config.version);
-        this.initialized = true;
         this.queue = {}
         this.currentSong = null
+        this.timeout = 0;
+
+        this.queueFetch();
+        this.dataFetch();
+        this.initialized = true;
+
 
         this.scheduleQueueFetch();
-
         if (!this.config.updateExternally) {
             this.scheduleDataFetch();
         }
@@ -51,14 +53,31 @@ Module.register("MMM-SonosQueue",{
     },
 
     renderQueue: function() { // Assume all data is updated fully
+        let maxTimeout = (1000 * 60 * 10) / this.config.queueUpdateInterval;
+        if (this.timeout > maxTimeout) {
+            return document.createElement("div");
+        }
+
         var queueList = document.createElement("span");
         queueList.className = "queue-list"
         queueList.id = this.identifier + "_queue";
         queueList.style.flexDirection = "column";
 
-        var length = Math.min(this.config.maxLength, Object.keys(this.queue).length);
-        //length = 10; //temp
-        for (var i = 0; i < length; i++) {
+        // Find currently playing song in queue
+        var currentSongPos = 0;
+        if (this.currentSong != null) {
+            for (var i = 0; i < Object.keys(this.queue).length; i++) {
+                var song = this.queue[i]
+                if (song.title == this.currentSong) {
+                    currentSongPos = i;
+                    break;
+                }
+            }
+        }
+
+        let numSongsRemaining = Object.keys(this.queue).length - currentSongPos - 1;
+        var length = Math.min(this.config.maxLength, numSongsRemaining);
+        for (var i = currentSongPos + length; i > currentSongPos; i--) {
             var song = this.queue[i];
             queueList.appendChild(this.createQueueItem(song, i))
         }
@@ -70,9 +89,6 @@ Module.register("MMM-SonosQueue",{
         var item = document.createElement("span");
         item.className = "queue-item";
         item.id = this.identifier + "_song_" + num;
-        item.style.borderColor = "white";
-		item.style.minWidth = self.config.minWidth;
-        item.style.minHeight = self.config.minHeight;
 
         var albumArt = document.createElement("span");
         albumArt.className = "play-symbol fa fa-pause";
@@ -103,6 +119,7 @@ Module.register("MMM-SonosQueue",{
 
     getCoverArt: function(url) {
         let coverArea = document.createElement('div');
+        coverArea.className = "album-art";
         let cover = document.createElement('img');
         cover.src = "http://10.0.0.15:1400" + url;
         cover.className = "album-art";
@@ -111,20 +128,16 @@ Module.register("MMM-SonosQueue",{
         return coverArea;
     },
 
-    processData: function(data) {
+    processStatus: function(data) {
         for (var i in data) {
             var group = data[i];
             var members = group.members;
             for (var j in members) {
                 var member = members[j]
-                for (var num in this.config.buttons) { // Look through each room specified in config
-                    var buttonRoomName = this.config.buttons[num].room;
-                    if (buttonRoomName == member.roomName) { // Find the button that matches this member
-                        if (member.state.playbackState == "PLAYING") {
-                            this.rooms[num].playing = true;
-                        } else {
-                            this.rooms[num].playing = false;
-                        }
+                if (this.config.room == member.roomName) { // Find the button that matches this member
+                    this.currentSong = member.state.currentTrack.title
+                    if (member.state.playbackState == "PLAYING") {
+                        this.timeout = 0; // Reset timeout if queue is actively playing music
                     }
                 }
             }
@@ -132,23 +145,8 @@ Module.register("MMM-SonosQueue",{
         this.updateDom();
     },
 
-    setCoordinator: function() {
-        var coordinatorFound = false;
-        for (var num in this.rooms) {
-            var room = this.rooms[num];
-            if (room.playing) {
-                this.coordinator = num;
-                coordinatorFound = true;
-            }
-        }
-
-        if (!coordinatorFound) {
-            this.coordinator = null;
-        }
-    },
-
     socketNotificationReceived: function(notification, payload) {
-        if (notification === "SONOS_STATUS") {
+        if (notification === "SONOS_DATA" && !this.config.updateExternally) {
             this.processStatus(payload);
         } else if (notification == "SONOS_QUEUE") {
             this.queue = payload;
@@ -156,21 +154,38 @@ Module.register("MMM-SonosQueue",{
         }
     },
 
-    scheduleQueueFetch: function(data) {
-        // Update current song every x seconds
+    notificationReceived: function(notification, payload) {
+        if (this.config.updateExternally) {
+            if (notification == "SONOS_ZONE_DATA") {
+                this.processStatus(payload);
+            }
+        }
+    },
+
+    queueFetch: function() {
+        var url = this.config.serverIP + "/" + this.config.room + "/queue";
+        this.sendSocketNotification('UPDATE_QUEUE', url);
+    },
+
+    dataFetch: function() {
+        var url = this.config.serverIP + "/zones";
+        this.sendSocketNotification('UPDATE_STATUS', url);
+    },
+
+    scheduleQueueFetch: function() {
+        // Update current song every x se ds
         var self = this;
         setInterval(() => {
-            var url = self.config.serverIP + "/" + self.config.room + "/queue";
-            this.sendSocketNotification('UPDATE_QUEUE', url);
+            self.queueFetch();
+            this.timeout += 1;
         }, this.config.queueUpdateInterval * 1000);
     },
 
-    scheduleDataFetch: function(data) {
+    scheduleDataFetch: function() {
         // Update current song every x seconds
         var self = this;
         setInterval(() => {
-            var url = self.config.serverIP + "/" + self.config.room + "/status";
-            this.sendSocketNotification('UPDATE_STATUS', url);
+            self.dataFetch();
         }, this.config.statusUpdateInterval * 1000);
     }
 
